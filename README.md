58e4555c83411a5f657b59525b914db4497bc51c
LeVieilEniac <tflahaul@gmail.com>
2019-08-24 12:35:27 +0200

    Modifications, restrict keyword, comments and pure attribute

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## How to use
 As a single header library, no build is required to use libsimd.
+
 Simply `#include "libsimd.h"` and specify `-msse4` in your compilation flags.
 
 
 ## Compilers requirements
-clang : 3.8 or newer
-gcc   : 4.9 or newer
+Clang : 3.8 or newer
+
+GCC   : 4.9 or newer
--- libsimd.h
+++ libsimd.h
@@ -32,10 +32,17 @@
 #   define __unlikely(x)			(x)
 #  endif /* __CCV */
 
+#  if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
+#   define __restrict				restrict
+#  else
+#   define __restrict // Ignore
+#  endif /* __STDC_VERSION__ */
+
 # else
 #  error "Your compiler does not use the C preprocessor, try using GCC"
 # endif /* __GNUC__ */
 
+# define __pure						__attribute__((pure))
 # define __isaligned(ptr, x)		(!((size_t)ptr & (x - 1)))
 
 # define FLAG_SBL	(_SIDD_SBYTE_OPS | _SIDD_CMP_EQUAL_EACH | _SIDD_LEAST_SIGNIFICANT)
@@ -45,17 +52,18 @@
 /*
 **	Strings
 */
-size_t					ft_strlen(char const *str)
+
+/* Return the length of @str */
+__pure size_t			ft_strlen(char const *__restrict str)
 {
 	char				*ptr = (char *)str;
 	__m128i				chunk;
-	__m128i				*longword;
+	__m128i				*longword = (__m128i *)__builtin_assume_aligned(ptr, M128_SZ);
 	__m128i const		set = _mm_setzero_si128();
 
 	if (__unlikely(!str[0]))
 		return (0);
-	longword = (__m128i *)__builtin_assume_aligned(ptr, M128_SZ);
-	while (longword)
+	for (;; longword++)
 	{
 		chunk = _mm_loadu_si128(longword);
 		if (_mm_cmpistrc(set, chunk, FLAG_SBL))
@@ -63,31 +71,31 @@ size_t					ft_strlen(char const *str)
 			ptr = (char *)longword + _mm_cmpistri(set, chunk, FLAG_SBL);
 			return ((size_t)(ptr - (char *)str));
 		}
-		++longword;
 	}
 	return (0);
 }
 
-char					*ft_strchr(char const *str, int c)
+/* Find the first occurrence of @c in @str */
+__pure char				*ft_strchr(char const *__restrict str, int c)
 {
 	__m128i				chunk;
 	__m128i				*longword = (__m128i *)__builtin_assume_aligned(str, M128_SZ);
 	__m128i const		set = _mm_setr_epi8(c, 0, 0, 0, 0, 0, 0, 0,
 											0, 0, 0, 0, 0, 0, 0, 0);
 
-	while (longword)
+	for (;; longword++)
 	{
 		chunk = _mm_loadu_si128(longword);
 		if (_mm_cmpistrc(set, chunk, FLAG_SBM))
 			return ((char *)longword + _mm_cmpistri(set, chunk, FLAG_SBM));
 		else if (_mm_cmpistrz(set, chunk, FLAG_SBM))
 			break ;
-		++longword;
 	}
 	return (NULL);
 }
 
-char					*ft_strrchr(char const *str, int c)
+/* Find the last occurrence of @c in @str */
+__pure char				*ft_strrchr(char const *__restrict str, int c)
 {
 	char				*ptr = NULL;
 	__m128i				chunk;
@@ -95,14 +103,13 @@ char					*ft_strrchr(char const *str, int c)
 	__m128i const		set = _mm_setr_epi8(c, 0, 0, 0, 0, 0, 0, 0,
 											0, 0, 0, 0, 0, 0, 0, 0);
 
-	while (longword)
+	for (;; longword++)
 	{
 		chunk = _mm_loadu_si128(longword);
 		if (_mm_cmpistrc(set, chunk, FLAG_SBM))
 			ptr = (char *)longword + _mm_cmpistri(set, chunk, FLAG_SBM);
 		else if (_mm_cmpistrz(set, chunk, FLAG_SBM))
 			break ;
-		++longword;
 	}
 	return (ptr);
 }
@@ -110,60 +117,74 @@ char					*ft_strrchr(char const *str, int c)
 /*
 **	Memory
 */
-void					ft_bzero(void *ptr, size_t size)
+
+/* Set @size bytes of @ptr to 0 */
+void					ft_bzero(void *__restrict ptr, size_t size)
 {
-	unsigned char		*mem;
+	uint8_t				*mem;
 	__m128i				*longword = (__m128i *)__builtin_assume_aligned(ptr, M128_SZ);
 	__m128i const		set = _mm_setzero_si128();
 
-	while (size >= M128_SZ)
-	{
+	for (; size >= M128_SZ; size -= M128_SZ)
 		_mm_storeu_si128(longword++, set);
-		size -= M128_SZ;
-	}
-	mem = (unsigned char *)longword;
+	mem = (uint8_t *)longword;
 	while (size-- > 0)
 		*mem++ = 0;
 }
 
-void					ft_memcpy(void *dst, void *src, size_t len)
+/* Copy the first @size bytes of @src to @dst */
+void					ft_memcpy(void *__restrict dst, void *__restrict src, size_t size)
 {
-	unsigned char		*ptd;
-	unsigned char		*pts;
+	uint8_t				*ptd, *pts;
 	__m128i				*dstlongword = (__m128i *)__builtin_assume_aligned(dst, M128_SZ);
 	__m128i				*srclongword = (__m128i *)__builtin_assume_aligned(src, M128_SZ);
 
-	while (len >= M128_SZ)
-	{
+	for (; size >= M128_SZ; size -= M128_SZ)
 		_mm_storeu_si128(dstlongword++, _mm_loadu_si128(srclongword++));
-		len -= M128_SZ;
-	}
-	ptd = (unsigned char *)dstlongword;
-	pts = (unsigned char *)srclongword;
-	while (len-- > 0)
+	ptd = (uint8_t *)dstlongword;
+	pts = (uint8_t *)srclongword;
+	while (size-- > 0)
 		*ptd++ = *pts++;
 }
 
-void					*ft_memset(void *dst, int c, size_t size)
+/* Set the first @size bytes of @dst to @c */
+void					*ft_memset(void *__restrict dst, int c, size_t size)
 {
-	unsigned char		*ptr;
+	uint8_t				*ptr;
 	__m128i				*longword = (__m128i *)__builtin_assume_aligned(dst, M128_SZ);
 	__m128i const		set = _mm_set1_epi8(c);
 
-	while (size >= M128_SZ)
-	{
+	for (; size >= M128_SZ; size -= M128_SZ)
 		_mm_storeu_si128(longword++, set);
-		size -= M128_SZ;
-	}
-	ptr = (unsigned char *)longword;
+	ptr = (uint8_t *)longword;
 	while (size-- > 0)
 		*ptr++ = c;
 	return (dst);
 }
 
+/* Compare the first @size bytes of @a and @b. Returns a boolean. */
+/*
+int						ft_memcmp(void *__restrict a, void *__restrict b, size_t size)
+{
+	uint8_t				*a8, *b8;
+	__m128i				*a128 = (__m128i *)__builtin_assume_aligned(a, M128_SZ);
+	__m128i				*b128 = (__m128i *)__builtin_assume_aligned(b, M128_SZ);
+
+	for (; size >= M128_SZ; size -= M128_SZ)
+		if (0x2a)
+			return (1);
+	a8 = (uint8_t *)a128;
+	b8 = (uint8_t *)b128;
+	while (size-- > 0)
+		if (*a8++ != *b8++)
+			return (1);
+	return (0);
+}
+*/
 /*
 **	Bits
 */
+
 inline uint16_t			ft_swap_uint16(uint16_t nb)
 {
 	return (((nb & 0x00ff) >> 8) | (uint16_t)((nb & 0xff00) << 8));
